6d5787ba742816db017cac1be3d25693f34e8dca
ahmad <ahmadalzoubi6446@gmail.com>
2021-05-28 17:54:41 +0300

    save

--- lib/screens/logIn_Screen.dart
+++ lib/screens/logIn_Screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../constants.dart';
